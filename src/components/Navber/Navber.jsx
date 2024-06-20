c383b05ef2280b76f0f83b38c7a75faf0cf11067
Shailesh kale <shaileshkale87730@gmail.com>
2024-06-20 23:09:09 +0530

    adding icon and main navber

--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -1,8 +1,40 @@
 import React, { useState } from "react";
-
-const Navber = () => {
+import rofImage from "../../assets/ROf Image 1.svg";
+import navicon1 from "../../assets/Vector (2).svg";
+import navicon2 from "../../assets/Vector (2)-2.svg";
+import carbon_customer1 from "../../assets/carbon_customer.svg";
+import carbon_customer2 from "../../assets/carbon_customer - Copy.svg";
+const Navber = ({ clickOverview = false }) => {
   return (
-    <div className="bg-[#FFFFFF] h-screen text-white flex flex-col pt-5 gap-9 items-center justify-center"></div>
+    <div className="bg-[#FFFFFF] h-screen text-white flex flex-col pt-5 gap-9 items-center justify-center">
+      <div className="flex flex-col gap-5 ">
+        <img src={rofImage} alt="ROF" />
+        <ul>
+          <li
+            className={`text-[#3D2314] font-medium flex flex-row gap-3 w-48 p-2 ${
+              clickOverview === true ? "bg-[#3D2314] text-[#FFFFFF]" : ""
+            }`}>
+            {clickOverview === true ? (
+              <img src={navicon2} alt="" />
+            ) : (
+              <img src={navicon1} alt="" />
+            )}{" "}
+            Overview
+          </li>
+          <li
+            className={`text-[#3D2314] font-medium flex flex-row gap-3 w-48 p-2 ${
+              clickOverview === true ? "bg-[#3D2314] text-[#FFFFFF]" : ""
+            }`}>
+            {clickOverview === true ? (
+              <img src={carbon_customer1} alt="" />
+            ) : (
+              <img src={carbon_customer2} alt="" />
+            )}{" "}
+            Direct Visitors
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- src/components/TopNavber/TopNavber.jsx
+++ src/components/TopNavber/TopNavber.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-
+import userImg from "../../assets/A1.webp";
 const TopNavber = () => {
+  const [title, setTitle] = useState("Super Admin");
+
   return (
     <div
-      className={`w-full h-16 bg-[#FFFFFF] flex flex-row justify-between px-5 items-center `}></div>
+      className={`w-full h-[4.688rem] bg-[#FFFFFF] flex flex-row justify-end px-5 gap-5 items-center font-[Manrope] font-medium`}>
+      <div>Welcome! {title}</div>
+      <div>
+        <img src={userImg} alt="Userimg" className="w-8 rounded-full mr-12" />
+      </div>
+    </div>
   );
 };
 
