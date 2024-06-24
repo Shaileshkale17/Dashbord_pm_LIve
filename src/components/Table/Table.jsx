9c32da99fd063f001bb9eee824bedceeacbf528c
“shailesh <shaileshkale87730@gmail.com>
2024-06-24 20:28:00 +0530

    adding functionality in search

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,29 +1,87 @@
-
-
+import { useState } from "react";
+import Searchsvg from "../../assets/material-symbols_search.svg";
 const Table = () => {
-  const visitors = Array(10).fill({
-    date: "26 Jun | 05:33 PM",
-    customerId: "ROFC001",
-    fullName: "Sameer Kashyap",
-    email: "SameerK123@gmail.com",
-    mobile: "+91 9254962485",
-    project: "Project A",
-    team: "Team A",
-    attendant: "Somesh Reddy",
-  });
+  // const visitors = Array(5).fill({
+  //   date: "26 Jun | 05:33 PM",
+  //   customerId: "ROFC001",
+  //   fullName: "Sameer Kashyap",
+  //   email: "SameerK123@gmail.com",
+  //   mobile: "+91 9254962485",
+  //   project: "Project A",
+  //   team: "Team A",
+  //   attendant: "Somesh Reddy",
+  // });
+
+  const visitors = [
+    {
+      date: "26 Jun | 05:33 PM",
+      customerId: "ROFC001",
+      fullName: "Sameer Kashyap",
+      email: "SameerK123@gmail.com",
+      mobile: "+91 9254962485",
+      project: "Project A",
+      team: "Team A",
+      attendant: "Somesh Reddy",
+    },
+    {
+      date: "26 Jun | 05:33 PM",
+      customerId: "ROFC002",
+      fullName: "Sameer Kashyap",
+      email: "SameerK123@gmail.com",
+      mobile: "+91 9254962485",
+      project: "Project A",
+      team: "Team A",
+      attendant: "Somesh Reddy",
+    },
+    {
+      date: "26 Jun | 05:33 PM",
+      customerId: "ROFC003",
+      fullName: "Sameer Kashyap",
+      email: "SameerK123@gmail.com",
+      mobile: "+91 9254962485",
+      project: "Project A",
+      team: "Team A",
+      attendant: "Somesh Reddy",
+    },
+    {
+      date: "26 Jun | 05:33 PM",
+      customerId: "ROFC004",
+      fullName: "Sameer Kashyap",
+      email: "SameerK123@gmail.com",
+      mobile: "+91 9254962485",
+      project: "Project A",
+      team: "Team A",
+      attendant: "Somesh Reddy",
+    },
+  ];
+
+  const [valueinput, setvalueinput] = useState("");
 
   return (
-    <div className="p-4 overflow-x-auto">
+    <div className="p-4 overflow-x-auto flex flex-col gap-9">
+      <h1 className="font-bold mt-8">
+        Home / <span className="font-medium">Direct Visitors</span>
+      </h1>
 
-   
-        <h1 className="font-bold mb-10">Home <span className="font-medium">Direct Visitors</span></h1>
-    
-
-      <div className="flex items-center justify-center ">
-        <input className=" mb-16 w-[60%] py-2 rounded-full px-10 outline-none border-2 border-gray-600 " type="text" name="" id="" placeholder="Search" />
+      <div className="flex flex-row  items-center justify-center">
+        <div className="relative w-[36rem] rounded-full">
+          <input
+            className="w-full py-2 px-12 rounded-full"
+            style={{ border: "2px solid #3D2314" }}
+            type="text"
+            name=""
+            id=""
+            value={valueinput}
+            onChange={(e) => setvalueinput(e.target.value)}
+            placeholder="Search"
+          />
+          <img
+            src={Searchsvg}
+            alt="Searchsvg"
+            className="absolute top-2 left-4"
+          />
+        </div>
       </div>
-
-
 
       <table className="min-w-full bg-white">
         <thead>
@@ -40,22 +98,26 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          {visitors.map((visitor, index) => (
-            <tr className="text-[14px]" key={index}>
-              <td className="py-2 px-4 border-b">{visitor.date}</td>
-              <td className="py-2 px-4 border-b">{visitor.customerId}</td>
-              <td className="py-2 px-4 border-b">{visitor.fullName}</td>
-              <td className="py-2 px-4 border-b">{visitor.email}</td>
-              <td className="py-2 px-4 border-b">{visitor.mobile}</td>
-              <td className="py-2 px-4 border-b">{visitor.project}</td>
-              <td className="py-2 px-4 border-b">{visitor.team}</td>
-              <td className="py-2 px-4 border-b">{visitor.attendant}</td>
-              <td className="py-2 px-4 border-b">
-                <button className="text-blue-600 mr-2">Edit</button>
-                <button className="text-red-600">Delete</button>
-              </td>
-            </tr>
-          ))}
+          {visitors
+            .filter(({ customerId }) => {
+              return customerId.indexOf(valueinput) >= 0;
+            })
+            .map((visitor, index) => (
+              <tr className="text-[14px]" key={index}>
+                <td className="py-2 px-4 border-b">{visitor.date}</td>
+                <td className="py-2 px-4 border-b">{visitor.customerId}</td>
+                <td className="py-2 px-4 border-b">{visitor.fullName}</td>
+                <td className="py-2 px-4 border-b">{visitor.email}</td>
+                <td className="py-2 px-4 border-b">{visitor.mobile}</td>
+                <td className="py-2 px-4 border-b">{visitor.project}</td>
+                <td className="py-2 px-4 border-b">{visitor.team}</td>
+                <td className="py-2 px-4 border-b">{visitor.attendant}</td>
+                <td className="py-2 px-4 border-b">
+                  <button className="text-blue-600 mr-2">Edit</button>
+                  <button className="text-red-600">Delete</button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
